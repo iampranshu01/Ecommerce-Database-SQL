0da56264f1766cfeb7e363586170e33c733a2f02
Pranshu <pranshuyadav5456@gmail.com>
2026-09-25 11:32:03 +0530

    Add database schema

---
+++ Database_Schema.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE Shopx;
+USE Shopx;
+
+-- Customers Table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    email VARCHAR(140) UNIQUE,
+    city VARCHAR(50),
+    signup_date DATE
+);
+
+
+-- Products Table
+CREATE TABLE products (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(80),
+    category VARCHAR(50),
+    price DECIMAL(10,2),
+    stock INT
+);
+
+-- Orders Table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    order_status VARCHAR(30),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+
+-- Order Items Table
+CREATE TABLE order_items (
+    order_item_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+-- Payments Table
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    payment_mode VARCHAR(30),
+    amount DECIMAL(10,2),
+    payment_date DATE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
+
+
+
+
+
+
